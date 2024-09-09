6fc58780de1065cea20366e24491f80b71c4f9ac
Rafael <rcax146@gmail.com>
2024-09-09 13:41:12 -0300

    Finalizado Exercício 2
    Finalização do exercício 2 da lista 4

---
+++ Lista4/exercicio2.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 2</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="container">
+    <h1>Exercício 2</h1>
+    <form action="exercicio2resp.php" method="POST">
+        <div class="row">
+            <div class="col">
+                <label for="palavra" class="form-label">Informe uma palavra: </label>
+                <input type="text" class="form-control" name="palavra" id="palavra">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+        </div>
+    </form>
+  </body>
